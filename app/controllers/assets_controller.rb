304dc0620e14bc47e11b2808a71867b293ae8a67
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2014-05-05 23:05:20 +0300

    Implement ability to delete assets from within student profiles

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -5,4 +5,11 @@ class AssetsController < ApplicationController
     asset = @student.assets.build
     render :partial => 'assets/form', locals: { file_number: params[:file_number].to_i, asset: asset }
   end
+
+  def destroy
+    asset = Asset.find(params[:id])
+    @student = asset.student
+    asset.destroy
+    redirect_to @student
+  end
 end
